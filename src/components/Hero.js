9e3d1b7b80ea8902d276b382e9fc315df93e43e9
Lucas Henrique <lucasheriques@gmail.com>
2020-08-13 22:23:51 -0300

    Updating News component and adjusting some texts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -177,15 +177,14 @@ const Hero = ({ children }) => {
 
           <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                Auxíliando ao combate do vírus{"\n"}
-                <br className="xl:hidden" />
-                <span className="text-indigo-600">COVID-19</span>
+              <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-5xl whitespace-pre-wrap">
+                Auxiliando o combate do{" "}
+                <span className="text-indigo-600 sm:block pt-2">COVID-19</span>
               </h2>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 O projeto CADCOV-19 tem a proposta de oferecer um sistema online
-                para auxiliar no diagnóstico utilizando técnicas de inteligência
-                artificial e visão computacional.
+                para auxiliar no diagnóstico da COVID-19 utilizando técnicas de
+                inteligência artificial e visão computacional.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
--- src/components/News.js
+++ src/components/News.js
@@ -3,16 +3,24 @@ import G1_logo from "../images/G1_logo.svg"
 
 const News = ({ children }) => {
   return (
-    <div className="max-w-6xl sm:flex-row flex-grow mx-auto" id="news">
-      <div className="grid gap-4 grid-cols-1 sm:grid-rows-1 sm:grid-cols-1 mt-4 m">
-        <div className="grid items-center justify-center">
+    <div
+      className="max-w-6xl flex-grow flex-row-reverse mx-auto mt-4 grid sm:grid-cols-2 sm:grid-rows-1 justify-around items-center"
+      id="news"
+    >
+      <div>
+        <h2 className="px-4 text-2xl text-gray-500 sm:text-3xl">
+          Divulgação na mídia:
+        </h2>
+      </div>
+      <div className="grid gap-4 grid-cols-1 sm:grid-rows-1 sm:grid-cols-1">
+        <div className="grid items-center justify-center sm:justify-end">
           <a
             href="https://g1.globo.com/mg/minas-gerais/noticia/2020/06/16/pesquisadores-da-ufmg-propoem-metodo-computadorizado-para-diagnostico-da-covid-19.ghtml"
             target="_blank"
             rel="noopener noreferrer"
           >
             <img
-              className="p-4 mb-5"
+              className="p-4 mb-4 sm:max-w-sm max-w-2"
               src={G1_logo}
               width="150"
               alt="noticia_g1"
